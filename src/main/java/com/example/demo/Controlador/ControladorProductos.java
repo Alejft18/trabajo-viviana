895b2d66b0b21b8970aab88af5347dbc74722788
Alejft18 <joseon85@gmail.com>
2023-03-25 18:11:06 -0500

    ingreso del crossOrigin

--- src/main/java/com/example/demo/Controlador/ControladorProductos.java
+++ src/main/java/com/example/demo/Controlador/ControladorProductos.java
@@ -2,12 +2,14 @@ package com.example.demo.Controlador;
 
 import com.example.demo.Entidades.Productos;
 import com.example.demo.Servicios.servicioProductos;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
+
 public class ControladorProductos {
     servicioProductos servicio = new servicioProductos();
 
